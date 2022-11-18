c2e5713cff603090a590adab25a2f06c71715f14
439774 <therealkimbladi@gmail.com>
2022-11-18 13:51:08 +0000

    Problem fixed

--- open_url.py
+++ open_url.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
 
 driver.get('https://sso.teachable.com/secure/teachable_accounts/sign_in')
+
+title = driver.title
 
 time.sleep(5)
 
