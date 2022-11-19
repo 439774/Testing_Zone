85318c69e48cdbf4c518f71c9af5ef49401d9b9b
439774 <95512181+439774@users.noreply.github.com>
2022-11-18 22:02:26 -0600

    Update interacting_browser.py

--- interacting_browser.py
+++ interacting_browser.py
@@ -1,10 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
 
 driver.get('https://sso.teachable.com/secure/teachable_accounts/sign_in')
 
-time.sleep(5)
+title = driver.title
 
-element_teach = driver.find_element(By.NAME, "teachable_account[email]")
+time.sleep(3)
+
+element_fill_user = driver.find_element(by=By.ID, value="teachable_account_email")
+element_fill_user.send_keys(input("please enter you're username for teachable! "))
+
+time.sleep(3)
+
+element_fill_pass = driver.find_element(by=By.ID, value="teachable_account_password")
+element_fill_pass.send_keys(input("Please enter you're password for teachable!"))
+click_button = driver.find_element(by=By.NAME, value="commit")
+click_button.click()
+
+time.sleep(5)
